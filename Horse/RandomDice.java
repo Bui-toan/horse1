c431a37ed1d9fed5534ebfc59d9aaf6bd7802ba1
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2024-05-02 18:52:00 +0700

    Update RandomDice.java

--- Horse/RandomDice.java
+++ Horse/RandomDice.java
@@ -1,7 +1,7 @@
 package Horse;
 import java.util.Random;
 public class RandomDice extends GameHorse {
-      private int scores;
+      private int scores=0;
       private void ThrowDice() {
     	  Random random=new Random();
     	  scores=(random.nextInt(6)+1);
