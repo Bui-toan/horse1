60c6c26cf3d71b1a6849e387d3308996e93ac0f9
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2024-05-02 18:54:06 +0700

    Update Player.java

--- Horse/Player.java
+++ Horse/Player.java
@@ -1,10 +1,12 @@
 package Horse;
 
+import Horse.GameMap;
+
 public class Player extends GameHorse {
       int color;
       static final int Horse=4;
       HorseBarn horsebarn =new HorseBarn(color);
-      des des =new des(color);
+      Des des =new Des(color);
       HorseSea horse[]= new HorseSea[Horse];
       Player(int color){
     	  this.color=color;
@@ -15,6 +17,23 @@ public class Player extends GameHorse {
       public void addHorse(int id) {
     	  horse[id]=new HorseSea(color,id);
       }
-      
-      
-}
+      public void addMouseListener(GameMap map, int steps){
+  		for(int i = 0; i < Horse; i++){
+  			if(horse[i] != null){
+  				horse[i].addMouseListener(map, steps);
+  			}	
+  		}
+  	}
+
+  	public void removeMouseListener(){
+  		for(int i = 0; i < Horse; i++){
+  			if(horse[i] != null){
+  				horse[i].RemoveMouseListener();
+  			}	
+  		}
+  	}
+
+  	public  boolean isWin(){
+  		return des.isWin();
+  	}
+  }
