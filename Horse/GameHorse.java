8ff54c2632a63d21a62690e0b23164a53a0ae1f3
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2024-05-09 00:52:35 +0700

    Update GameHorse.java

--- Horse/GameHorse.java
+++ Horse/GameHorse.java
@@ -3,14 +3,14 @@ import javax.swing.JOptionPane;
 import java.util.concurrent.Semaphore;
 
 public class GameHorse {
-      static final int RED=4;
-      static final int YELLOW=2;
-      static final int BLUE=1;
-      static final int GREEN=3;
-      static final int DISTANCES=50;
+	static final int BLUE = 1;
+	static final int RED = 4;
+	static final int YELLOW = 2;
+	static final int GREEN = 3;
+	static final int DISTANCES = 45;
       
-      static boolean ThrowPhaseFlag=true; //tung xúc sắc
-      static boolean HorseFLAG =false;   // đi cá ngựa
+      static boolean ThrowPhaseFlag=true; 
+      static boolean HorseFLAG =false;
       static Semaphore throwFlagSema =new Semaphore(0);
       static Semaphore HorseFlagSema= new Semaphore(0);
       public void Error(String error) {
