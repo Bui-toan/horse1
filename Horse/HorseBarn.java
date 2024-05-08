c59b736a2d9c7c372cc9361ee06b145c4edbc292
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2024-05-09 00:54:58 +0700

    Update HorseBarn.java

--- Horse/HorseBarn.java
+++ Horse/HorseBarn.java
@@ -1,38 +1,42 @@
 package Horse;
+public class HorseBarn extends GameHorse{
+	/* Chuồng */
+	private int number = 0;
+	static final int NO_HORSE = -1;
+	private boolean id[] = new boolean[Player.Horse]; 
 
-public class HorseBarn extends GameHorse {
-      private int NumberHorse=0; //so ngựa trong chuồng
-      static final int No_Horse=-1;
-      private boolean id[]=new boolean [Player.Horse]; //vị trí cá ngựa trong chuồng
-      
-      HorseBarn(int color) {
-    	  NumberHorse=Player.Horse;
-    	  for(int i=0;i<Player.Horse;i++) {
-    		  id[i]=true;
-    	  }
-      }
-      public void add(int id) { //thêm ngựa vào chuồng
-    	  NumberHorse++;
-    	  this.id[id]=true;
-      }
-      public int getHorse() { //lấy thông tin nguựa trong chuồng
-    	  if(isEmpty()) {
-    		  return No_Horse;
-    	  }
-    	  
-    		  for(int i=0;i<Player.Horse;i++) {
-    			  if(id[i]=true) {
-    				  return i;
-    			  }
-    		  }
-    	  
-    	  return No_Horse;
-      }
-      public void remove(int id) {// xóa ngựa khỏi chuồng
-    	  NumberHorse--;
-    	  this.id[id]=false;
-      }
-      public boolean isEmpty() {
-    	  return NumberHorse==0;
-      }
+	HorseBarn(int color){
+		number = Player.Horse;
+		for(int i = 0; i < Player.Horse; i++){
+			id[i] = true;
+		}
+	}
+
+	public void add(int id){
+		number++;
+		this.id[id] = true;
+	}
+
+	public int getHorse(){
+		if(isEmpty()){
+			return NO_HORSE;
+		}
+
+		for(int i = 0; i < Player.Horse; i++){
+			if(id[i] == true){
+				return i;
+			}
+		}
+		
+		return NO_HORSE;
+	}
+
+	public void remove(int id){
+		number--;	
+		this.id[id] = false;
+	}
+
+	public boolean isEmpty(){
+		return number == 0;
+	}
 }
