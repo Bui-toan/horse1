977081b2e53b31548dfd16d9dce705467631b85c
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2024-05-10 22:30:32 +0700

    Update GameSession.java

--- Horse/GameSession.java
+++ Horse/GameSession.java
@@ -1,17 +1,10 @@
 package Horse;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
-
 import SQL.Information;
-
+import File.FileHorse;
 public class GameSession extends GameHorse {
 	private final int ONE_BONUS = -1;
 	private final int NO_BONUS = 0;
@@ -84,60 +77,23 @@ public class GameSession extends GameHorse {
 			if(map.isWin()){
 				endGameFlag = true;
 				try {
-					saveFile(graphic.timeString,map.playerwin);
+					FileHorse.saveFile(graphic.timeString,map.playerwin);
+					Information.History(map.playerwin,graphic.timeString);
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				Information.History(map.playerwin,graphic.timeString);
+				
 			}
 		}
 	}
-	 public void saveFile(String time, int color) throws IOException {
-	        String content = "\nLịch sử trò chơi\n";
-	        String filename = "history.txt";
-	        clearFile(filename); // Gọi hàm clearFile để xóa hết dữ liệu trong file
-	        FileWriter fw = null;
-	        BufferedWriter bw = null;
-	        try {
-	            File file = new File(filename);
-	            if (!file.exists()) {
-	                file.createNewFile(); // Tạo file mới nếu không tồn tại
-	            }
-	            clearFile(filename);
-	            fw = new FileWriter(file, true);
-	            bw = new BufferedWriter(fw);
-	            bw.write(content);
-	            bw.write("Thời gian: " + time);
-	            bw.write("\nNgười chơi " + color + " chiến thắng.\n");
-	            bw.write("-------------------------------------");
-	        } catch (IOException e) {
-	            throw new RuntimeException(e);
-	        } finally {
-	            try {
-	                if (bw != null)
-	                    bw.close();
-	                if (fw != null)
-	                    fw.close();
-	            } catch (IOException ex) {
-	                ex.printStackTrace();
-	            }
-	        }
-	    }
-
-	    // Phương thức clearFile để xóa hết dữ liệu trong file
-	    public void clearFile(String fileName) {
-	        try (PrintWriter writer = new PrintWriter(fileName)) {
-	            // Ghi dữ liệu trống vào file
-	            writer.print("");
-	        } catch (FileNotFoundException e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
+	 
 
 	public static void main(String args[]) throws SQLException {
 		GameSession session = new GameSession();
 		session.playGame();
+		if(GameGraphic.isGameRunning=false) {
+			Information.closeConnection();
+		}
 	}
 
 }
